7b518f0bda2367d008ed05c4f15a75c61fe1773a
alex <alex@poissin.com>
2016-11-08 16:17:47 +1000

    more cleanup

---
+++ ploe.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  
+  FILE *gp1 = popen("gnuplot -persist","w");
+  char * cmds[] = { "set term pdf", "set yrange [0:]","plot \"plote.txt\" with lines" };
+
+  for (int i=0;i<3;i++)
+    fprintf(gp1,"%s\n",cmds[i]);
+
+  fclose(gp1);  
+
+  return 0;
+}
