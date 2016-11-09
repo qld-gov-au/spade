1734c1855c5bd365a9a1f5f3438ebaeccd5fc8f3
alex <alex@poissin.com>
2016-11-09 17:34:20 +1000

    converging to minimal curvature

--- spade.c
+++ spade.c
@@ -647,6 +647,31 @@ int main(int argc, char *argv[])
   s = gsl_multimin_fminimizer_alloc(T,2);
   
   gsl_multimin_fminimizer_set (s,&minex,qq,ss);
+
+
+  do
+    {
+      iter++;
+      status = gsl_multimin_fminimizer_iterate(s);
+      
+      if (status) 
+        break;
+
+      size = gsl_multimin_fminimizer_size (s);
+      status = gsl_multimin_test_size (size, 1e-2);
+
+      if (status == GSL_SUCCESS)
+        {
+          printf ("converged to minimum at\n");
+        }
+
+      printf ("%5d %10.3e %10.3e f() = %7.3f size = %.3f\n", 
+              iter,
+              gsl_vector_get (s->x, 0), 
+              gsl_vector_get (s->x, 1), 
+              s->fval, size);
+    }
+  while (status == GSL_CONTINUE && iter < 100);
   
   /*
   
